bce954fd7aeb15110bd63cb4e60043999cd2571c
Felipe <falmeidagomes13@gmail.com>
2023-02-01 21:37:02 -0300

    Math functions incremented

--- src/Components/Calculadora.jsx
+++ src/Components/Calculadora.jsx
@@ -7,6 +7,7 @@ function Calculadora() {
   const [firstStored, setFirstStored] = useState(0)
   const [value, setValue] = useState(0)
   const [sequence, setSequence] = useState(false)
+  const [operation, setOperation] = useState(null)
 
   function clearValue() {
     setValue(0)
@@ -14,71 +15,86 @@ function Calculadora() {
     setSequence(false)
   }
 
-  function operation(label) {
+  function mathOperation(label) {
+    setOperation(label)
+
+    if (sequence === true) {
+      return
+    }
     if (firstStored === 0) {
-      setFirstStored(parseFloat(value))
+      setFirstStored(value)
       setValue(0)
       return
     }
-    setValue(firstStored + value)
+    setValue(eval(firstStored + operation + value))
+    setOperation(label)
     setSequence(true)
   }
-  
+
   function insertNum(label) {
-    const valueToStrig = value.toString()
-    if (label === ',' && valueToStrig == '0.') {
-      console.log('aqui')
+    const valueToString = value.toString()
+    if (sequence === true && label === '.' && value !== '0.') {
+      setFirstStored(value)
+      setValue('0.')
       return
     }
-    if (label === ',' && !valueToStrig.includes('.')) {
+    if (label === '.' && valueToString.includes('.')) {
+      return
+    }
+    if (label === '.') {
       setValue(value + '.')
       return
     }
     if (value === 0) {
-      setValue(parseFloat(label))
+      setValue(label)
       return
     }
     if (sequence === true) {
-      console.log('aqui')
+      if (value === '0.') {
+        setValue(value + label)
+        setSequence(false)
+        return
+      }
       setFirstStored(value)
-      setValue(parseFloat(label))
+      setValue(label)
       setSequence(false)
       return
     }
-    setValue(parseFloat(value + label))
+    setValue(value + label)
   }
-  
+
   function equals() {
+    if (sequence === true) {
+      return
+    }
     if (firstStored === 0) {
       return
     }
-    setValue(value + firstStored)
+    setValue(eval(firstStored + operation + value))
     setFirstStored(0)
-    setSequence(true)
+    setOperation(null)
   }
-
-
 
 
   return (
     <h1 className='calculadora'>
-      <Display value={value} />
+      <Display value={value < 1 ? value : value % 1 === 0 ? value : value.toFixed(2)} />
       <Button label="AC" triple click={clearValue} />
-      <Button label="/" operator click={operation} />
+      <Button label="/" operator click={mathOperation} />
       <Button label="7" click={insertNum} />
       <Button label="8" click={insertNum} />
       <Button label="9" click={insertNum} />
-      <Button label="*" operator click={operation} />
+      <Button label="*" operator click={mathOperation} />
       <Button label="4" click={insertNum} />
       <Button label="5" click={insertNum} />
       <Button label="6" click={insertNum} />
-      <Button label="-" operator click={operation} />
+      <Button label="-" operator click={mathOperation} />
       <Button label="1" click={insertNum} />
       <Button label="2" click={insertNum} />
       <Button label="3" click={insertNum} />
-      <Button label="+" operator click={operation} />
+      <Button label="+" operator click={mathOperation} />
       <Button label="0" double click={insertNum} />
-      <Button label="," click={insertNum} />
+      <Button label="." click={insertNum} />
       <Button label="=" operator click={equals} />
     </h1>
   );
