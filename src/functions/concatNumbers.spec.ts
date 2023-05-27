10b19333ea7a3adf5be507d9f64b689260532774
felipe-AGomes <falmeidagomes13@gmail.com>
2023-05-27 15:13:06 -0300

    refactor: moved sut to separate file

--- src/functions/concatNumbers.spec.ts
+++
@@ -1,9 +0,0 @@
-import concatNumbers from './concatNumbers';
-
-describe('concatNumbers', () => {
-	it('should return two numbers concatenated', () => {
-		const result = concatNumbers(12, 22);
-
-		expect(result).toBe(1222);
-	});
-});
--- src/functions/concatNumbers.ts
+++
@@ -1,4 +0,0 @@
-export default function concatNumbers(n1: number, n2: number) {
-	const stringsConcated = n1.toString() + n2.toString();
-	return +stringsConcated;
-}
