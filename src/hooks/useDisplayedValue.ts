aad1686981fd4affb0011ee8586a0676d29751f2
felipe-AGomes <falmeidagomes13@gmail.com>
2023-05-29 08:48:07 -0300

    refactor: hook improvements

--- src/hooks/useDisplayedValue.ts
+++ src/hooks/useDisplayedValue.ts
@@ -3,35 +3,29 @@ import useCalculatorContext from './useCalculatorContext';
 export default function useDisplayedValue() {
 	const { displayedValue, setDisplayedValue, setLastValue } =
 		useCalculatorContext();
-
-	const clearDisplayValue = () => {
-		setDisplayedValue([]);
-		setLastValue(null);
-	};
-
 	const deleteOneDisplayValue = () => {
 		const newDisplayedValue = [...displayedValue];
 
 		if (newDisplayedValue.length === 0) {
-			setDisplayedValue([]);
 			return;
 		}
 
 		if (newDisplayedValue[newDisplayedValue.length - 1].length === 1) {
-			const result = newDisplayedValue.slice(0, -1);
-			setDisplayedValue(result);
+			newDisplayedValue.pop();
+			setDisplayedValue(newDisplayedValue);
 			return;
 		}
+
 		const lastElement =
 			newDisplayedValue[newDisplayedValue.length - 1].toString();
-
-		const modifiedLastElement = lastElement.slice(0, -1);
-
-		const newLastElement = modifiedLastElement;
-
-		newDisplayedValue[newDisplayedValue.length - 1] = newLastElement;
+		newDisplayedValue[newDisplayedValue.length - 1] = lastElement.slice(0, -1);
 
 		setDisplayedValue(newDisplayedValue);
+	};
+
+	const clearDisplayValue = () => {
+		setDisplayedValue([]);
+		setLastValue(null);
 	};
 
 	const handleSetDisplayedValue = ({
@@ -48,10 +42,6 @@ export default function useDisplayedValue() {
 		if (isNum) {
 			const newDisplayedValue = [...displayedValue];
 			const lastPosition = newDisplayedValue.length - 1;
-			if (isNaN(+newDisplayedValue[lastPosition])) {
-				setDisplayedValue([...displayedValue, value]);
-				return;
-			}
 			newDisplayedValue[lastPosition] = newDisplayedValue[lastPosition] + value;
 
 			setDisplayedValue(newDisplayedValue);
