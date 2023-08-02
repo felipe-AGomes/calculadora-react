aa46a9fa03fec3d7a4cc10aff760b1d4af87eb43
felipe-AGomes <falmeidagomes13@gmail.com>
2023-08-02 09:57:38 -0300

    add digital signature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { generateDigitalSignature } from 'assinatura-digital';
 import './app.css';
 import Calculator from './components/Calculator/Calculator';
 import useThemeContext from './hooks/useThemeContext';
 
 function App() {
 	const { theme } = useThemeContext();
+	console.clear();
+	const transformedSignature = generateDigitalSignature(
+		'felipe-dev',
+		'falmeidagomes13@gmail.com',
+		'Bem-vindo ao meu site! Sinta-se à vontade para explorar e desenvolver com paixão!',
+	);
 
+	console.log(...transformedSignature);
 	return (
 		<div className={`container__calculator ${theme}`}>
 			<Calculator />
